630ca000a5b323ef2d1ab8b2f996c154ba6df37a
Jerico Rangel <jerico.miggy.rangel@gmail.com>
2024-03-05 20:14:42 +0800

    Updates

---
+++ _app/immutable/nodes/3.1d05094f.js
@@ -0,0 +1,6 @@
+import{s as l,n as a}from"../chunks/scheduler.1448e862.js";import{S as r,i as o,g as c,h as m,x as h,k as d,a as g,f as p}from"../chunks/index.f9da1157.js";function v(n){let e,s=`<main class="svelte-1z0qfxk"><h1 class="svelte-1z0qfxk">About</h1> <p>I&#39;m a Data Engineer with capabilities in <b>Big Data </b>, <b>MLOps</b>, and self-serve <b>Analytics </b>.
+            Work experience has been in game development, research/academe, and telecommunications. I&#39;ve worked with all sorts of data, 
+            and my strengths are in problem solving, high level thinking, and communication. 
+            
+            <br/> <br/>
+            Outside work, I am a seasoned athlete -- weighted calisthenics, running, climbing, powerlifting 🏃‍♂️🏋️‍♂️--,gamer, and a cat lover 🐱.</p></main>`;return{c(){e=c("div"),e.innerHTML=s,this.h()},l(t){e=m(t,"DIV",{class:!0,"data-svelte-h":!0}),h(e)!=="svelte-46lbyy"&&(e.innerHTML=s),this.h()},h(){d(e,"class","container svelte-1z0qfxk")},m(t,i){g(t,e,i)},p:a,i:a,o:a,d(t){t&&p(e)}}}function f(n){return[]}class _ extends r{constructor(e){super(),o(this,e,f,v,l,{})}}export{_ as component};
---
+++ _app/immutable/nodes/5.554809c4.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.1448e862.js";import{S as t,i as e}from"../chunks/index.f9da1157.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
